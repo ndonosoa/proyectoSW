bb51258942af0db13e0d70f37acc5d8ef70bf332
Nataniel Donoso <n.donoso.a@gmail.com>
2016-12-12 00:33:18 -0300

    arreglado problema de jquery para ddl y datatable, agregado de forms a las views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_name: params[:user][:user_name],
+                          user_rut: params[:user][:user_rut],
+                          user_tel: params[:user][:user_tel],
+                          mail: params[:user][:email],
+                          user_range: params[:user][:user_range],
+                          encrypted_password: params[:user][:encrypted_password])
   end
 
   def show
