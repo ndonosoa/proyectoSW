5ae55754fece3ed368db75256b5a3ef670324942
Nataniel Donoso <n.donoso.a@gmail.com>
2017-02-25 18:52:32 -0300

    Agregado login falta terminar, redirecciones y panel de navegacion por tipo de usuario por terminar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :purchases
-  validates : rut_user, presence: true, confirmation: true, uniqueness: true
-  validates : email_user, presence: true, confirmation: true
+  
 end
